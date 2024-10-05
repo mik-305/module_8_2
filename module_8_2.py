0e4cf38d0c074e391a0d3e8ab9ad5ef9f0b3a5d9
mik-305 <mik-305@bk.ru>
2024-10-05 19:22:19 +0300

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -1,8 +1,8 @@
-result = 0                                  #   Предварительная версия на 4 окт
-incorrect_data = 0
 def personal_sum(self):
     global result
     global incorrect_data
+    result = 0
+    incorrect_data = 0
     for i in numbers:
         try:
             result += i
@@ -11,23 +11,26 @@ def personal_sum(self):
             incorrect_data +=1
         except IndexError:
             print('Неверный тип данных по индексу - >', i)
-    return print(result, incorrect_data)
 def calculate_average(self):
+    if type(numbers) == int: #  Проверка типа входных данных(коллекция или нет)
+       print('В numbers записан некорректный тип данных ') # На входе не коллеция
+       return               # Прекращаем выполение функции
     personal_sum(numbers)
     sredn = 0
     try:
         if result == 0:
-            print('("', numbers, '")')
-            print('Коллекция либо пуста, либо не содержит чисел :')
-            return
-        sredn = result / len(numbers)                   # Высичляем среднее значение по списку
-        print('Среднее значение :', sredn)
+            return result
+        sredn = result / (len(numbers) - incorrect_data)              # Cреднее значение по списку - только числа!)
     except ZeroDivisionError:
         print('Деление на ноль!')
-    return sredn
+    return sredn #result
 
 
 numbers = ("1, 2, 3")
-#print(f'Результат 1: {calculate_average(numbers)}')
-#numbers = ([1, "Строка", 3, "Ещё Строка"])
+print(f'Результат 1: {calculate_average(numbers)}')
+numbers = ([1, "Строка", 3, "Ещё Строка"])
 print(f'Результат 2: {calculate_average(numbers)}')
+numbers = (567)
+print(f'Результат 3: {calculate_average(numbers)}')
+numbers = [42, 15, 36, 13]
+print(f'Результат 4: {calculate_average(numbers)}')
